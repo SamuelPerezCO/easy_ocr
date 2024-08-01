35f13f5e466be70b5d577ef816313c3e2d607ff3
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-08-01 16:15:42 -0500

    primeros codigos

---
+++ extraccion.py
@@ -0,0 +1,33 @@
+import fitz  # PyMuPDF
+import easyocr
+import numpy as np
+from PIL import Image
+
+def extract_text_from_pdf(pdf_path, zoom_factor=2):
+    # Inicializar el lector de easyocr
+    reader = easyocr.Reader(['en'])  # Puedes agregar más idiomas si es necesario
+    pdf_document = fitz.open(pdf_path)
+    texts = []
+    
+    # Convertir cada página a una imagen y extraer el texto
+    for page_number in range(len(pdf_document)):
+        page = pdf_document.load_page(page_number)
+        mat = fitz.Matrix(zoom_factor, zoom_factor)
+        pix = page.get_pixmap(matrix=mat)
+        
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        img_np = np.array(img)
+        
+        result = reader.readtext(img_np)
+        page_text = "\n".join([text[1] for text in result])
+        texts.append(page_text)
+    
+    return texts
+
+# Uso de la función
+pdf_path = "ruta/al/archivo.pdf"
+texts = extract_text_from_pdf(pdf_path)
+
+# Imprimir el texto extraído de cada página
+for i, text in enumerate(texts):
+    print(f"Texto de la página {i + 1}:\n{text}\n")
---
+++ recorte.py
@@ -0,0 +1,27 @@
+import fitz  # PyMuPDF
+from PIL import Image
+
+def pdf_page_screenshot(pdf_path, page_number, output_image_path, zoom_factor=2):
+    # Abrir el documento PDF
+    pdf_document = fitz.open(pdf_path)
+    
+    # Obtener la página específica (número de página comienza desde 0)
+    page = pdf_document.load_page(page_number - 1)
+    
+    # Establecer el factor de zoom (resolución)
+    mat = fitz.Matrix(zoom_factor, zoom_factor)
+    
+    # Renderizar la página en una imagen (pixmap) con el factor de zoom
+    pix = page.get_pixmap(matrix=mat)
+    
+    # Guardar el pixmap como un archivo de imagen
+    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+    img.save(output_image_path, "PNG")
+
+# Uso de la función
+pdf_path = "/home/samuelperez/Codes/easy_ocr/Informe___El_futuro_de_la_banca_minorista_2030_.pdf"
+page_number = 17  # Número de la página que deseas capturar
+output_image_path = "pantallazo.png"
+zoom_factor = 3  # Factor de zoom para aumentar la resolución
+
+pdf_page_screenshot(pdf_path, page_number, output_image_path, zoom_factor)
